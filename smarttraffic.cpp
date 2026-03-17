0adee3fcec852c743137e2fe6422db088795113b
Anushka Bukkawar <anushkabukkawar64@gmail.com>
2026-03-17 11:32:45 +0530

    Refactor smart traffic control system for lanes

--- smarttraffic.cpp
+++ smarttraffic.cpp
@@ -1,48 +1,60 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int choice;
-    int vehicleCount = 0;
+    int lane1, lane2, lane3;
 
-    do
-    {
+    do {
         cout << "\n--- Smart Traffic Control System ---\n";
         cout << "1. Enter Traffic Data\n";
         cout << "2. View Signal Status\n";
         cout << "3. Exit\n";
-        cout << "Enter your choice: ";
+        cout << "Enter choice: ";
         cin >> choice;
 
-        switch(choice)
-        {
+        switch(choice) {
+
             case 1:
-                cout << "Enter number of vehicles on the lane: ";
-                cin >> vehicleCount;
+                cout << "\nEnter vehicles in Lane 1: ";
+                cin >> lane1;
+                cout << "Enter vehicles in Lane 2: ";
+                cin >> lane2;
+                cout << "Enter vehicles in Lane 3: ";
+                cin >> lane3;
                 break;
 
             case 2:
-                if(vehicleCount == 0)
-                {
-                    cout << "No traffic data available.\n";
-                }
-                else if(vehicleCount < 10)
-                {
-                    cout << "Normal Green Signal (30 seconds)\n";
-                }
-                else if(vehicleCount >= 10 && vehicleCount < 20)
-                {
-                    cout << "Green Signal Extended (45 seconds)\n";
-                }
+                cout << "\n--- Signal Status ---\n";
+
+                // Lane 1
+                if(lane1 > 40)
+                    cout << "Lane 1: Green Extended (60 sec)\n";
+                else if(lane1 > 20)
+                    cout << "Lane 1: Normal Green (40 sec)\n";
                 else
-                {
-                    cout << "Heavy Traffic - Green Signal Extended (60 seconds)\n";
-                }
+                    cout << "Lane 1: Short Green (20 sec)\n";
+
+                // Lane 2
+                if(lane2 > 40)
+                    cout << "Lane 2: Green Extended (60 sec)\n";
+                else if(lane2 > 20)
+                    cout << "Lane 2: Normal Green (40 sec)\n";
+                else
+                    cout << "Lane 2: Short Green (20 sec)\n";
+
+                // Lane 3
+                if(lane3 > 40)
+                    cout << "Lane 3: Green Extended (60 sec)\n";
+                else if(lane3 > 20)
+                    cout << "Lane 3: Normal Green (40 sec)\n";
+                else
+                    cout << "Lane 3: Short Green (20 sec)\n";
+
                 break;
 
             case 3:
-                cout << "Exiting system...\n";
+                cout << "Exiting program...\n";
                 break;
 
             default:
